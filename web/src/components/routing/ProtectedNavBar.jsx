53efff3d551c866c304bec63ecfdeed0732cc044
ECG <cgeulalia@gmail.com>
2025-09-27 23:12:50 +0000

    footer

--- web/src/components/routing/ProtectedNavBar.jsx
+++ web/src/components/routing/ProtectedNavBar.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import { NavBar } from '../NavBar';
+import { Footer } from '../Footer';
 
 export const ProtectedNavBar = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 };
