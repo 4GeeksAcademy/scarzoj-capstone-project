684746b7dc5da7ec734f545f12ed6890efda35c4
Daniela González <danimge94@gmail.com>
2025-09-30 17:19:19 +0000

    cambie el login para usar libreria de react bootstrap

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -1,36 +1,43 @@
-import { useContext, useState } from "react";
-import { Button, Container, TextField, FormGroup } from "@mui/material";
-import { UserContext } from "../context/User";
+import { useContext, useState } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+import { UserContext } from '../context/User';
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const { login } = useContext(UserContext);
 
   return (
-    <Container>
-      <FormGroup className="mb-3" controlId="formBasicEmail">
-        <TextField
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          label="Email address"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup className="mb-3" controlId="formBasicPassword">
-        <TextField
-          type="password"
-          placeholder="Password"
-          label="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormGroup>
-      <Button variant="primary" onClick={() => login(email, password)}>
-        Submit
-      </Button>
+    <Container style={{ maxWidth: '400px', marginTop: '50px' }}>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          className="w-100"
+          onClick={() => login(email, password)}
+        >
+          Submit
+        </Button>
+      </Form>
     </Container>
   );
 };
